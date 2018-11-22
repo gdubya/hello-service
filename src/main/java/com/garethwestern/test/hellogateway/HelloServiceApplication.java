a52c083749021d5301960ef57d031107fcd2d242
Gareth Western <gareth.western@bouvet.no>
2018-11-22 20:24:44 +0100

    say hello

--- src/main/java/com/garethwestern/test/hellogateway/HelloServiceApplication.java
+++ src/main/java/com/garethwestern/test/hellogateway/HelloServiceApplication.java
@@ -3,6 +3,7 @@ package com.garethwestern.test.hellogateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,4 +12,10 @@ public class HelloServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloServiceApplication.class, args);
 	}
+
+	@RequestMapping("/hello")
+	public String serviceInstancesByApplicationName() {
+		return "hello!";
+	}
+
 }
