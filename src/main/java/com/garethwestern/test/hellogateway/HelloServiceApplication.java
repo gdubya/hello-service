72b853cbee422b1aaf3d7a4067bb406ba458de9b
Gareth Western <gareth.western@bouvet.no>
2018-11-22 20:56:49 +0100

    set response body

--- src/main/java/com/garethwestern/test/hellogateway/HelloServiceApplication.java
+++ src/main/java/com/garethwestern/test/hellogateway/HelloServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -19,6 +20,7 @@ public class HelloServiceApplication {
 	class ServiceInstanceRestController {
 
 		@GetMapping("/hello/{name}")
+		@ResponseBody
 		public String hello(@PathVariable String name) {
 			return "Hello " + name;
 		}
